a3ed2d1719755347ad83c1e9f9f2e9fea9b671d1
Luke Millar <luketmillar@gmail.com>
2021-04-10 14:57:38 -0700

    Styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
 import React from 'react'
+import styled, { createGlobalStyle } from 'styled-components'
 import './App.css'
 
-function App() {
+const Name = styled.div`
+  font-weight: 900;
+  font-style: italic;
+  font-size: 6rem;
+  text-align: center;
+  line-height: .9em;
+`
+const Caption = styled.div`
+  font-weight: 400;
+  text-transform: uppercase;
+  fontSize: 1rem;
+  text-align: center;
+  letter-spacing: 6px;
+`
+
+const GlobalStyle = createGlobalStyle`
+  @media screen and (min-width: 900px) {
+    ${Name} {
+      font-size: 8rem;
+    };
+    ${Caption} {
+      letter-spacing: 16px;
+    }
+  }
+`
+
+const App = () => {
   return (
-    <div style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <h1>Luke Millar</h1>
-    </div>
+    <>
+      <GlobalStyle />
+      <div style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
+        <Name>Luke Millar</Name>
+        <Caption>Code · Games · Design · Teach</Caption>
+      </div>
+    </>
   )
 }
 
