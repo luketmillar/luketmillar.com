f94c3a7b7298df6b9499e0cb7cc13de7fc57ffa9
Luke Millar <luketmillar@gmail.com>
2021-04-11 22:20:30 -0700

    Animated gradient

--- src/home/sections/Projects/index.tsx
+++ src/home/sections/Projects/index.tsx
@@ -1,14 +1,48 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import Container from '../container'
 
 const ProjectsContainer = styled(Container)`
-    background: linear-gradient(to right top, #000, #9013fe);
     text-align: center;
+`
+
+const move = keyframes`
+    %0 {
+        transform: translate(-50%, 50%);
+    }
+    33% {
+        transform: translate(-50%, 30%);
+    }
+    67% {
+        transform: translate(-20%, 50%);
+    }
+    100% {
+        transform: translate(-50%, 50%);
+    }
+`
+
+const duration = 30
+
+const GradientBlob = styled.div<{ color: string, size: number }>`
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: ${props => props.size}vmax;
+    height: ${props => props.size}vmax;
+    transform: translate(-50%, 50%);
+
+    animation: ${move} ${duration}s linear infinite;
+
+    z-index: -1;
+
+    background: radial-gradient(${props => props.color}, rgba(0,0,0,0) 60%);
 `
 
 const ProjectsSection = () => {
     return <ProjectsContainer>
+        <GradientBlob size={110} color={"rgba(255,255,255,1)"} style={{ animationDelay: `${2 * duration / 3}s` }} />
+        <GradientBlob size={120} color={"#9013fe"} />
+        <GradientBlob size={140} color={"#D11DE1"} style={{ animationDelay: `${duration / 3}s` }} />
         <h1 style={{ lineHeight: '0.9em', fontWeight: 900, fontStyle: 'italic' }}>More coming soon</h1>
     </ProjectsContainer>
 }
