e5d296889d6a50c0c82991920792167fc69ecf50
Luke Millar <luketmillar@gmail.com>
2021-05-15 01:38:31 -0700

    World positioned balls

--- src/projects/bouncingBalls/World.ts
+++ src/projects/bouncingBalls/World.ts
@@ -1,14 +1,16 @@
+import { getWorldSize } from "projects/canvasScene/Coordinates"
 import { Gravity } from "projects/canvasScene/Model/Forces"
 import { World, Circle } from "../canvasScene/Model"
 
 const colors = ['#fff', '#f00', '#0f0', '#00f', '#0ff', '#ff0', '#f0f']
 
 const createCircles = (n: number) => {
+    const worldSize = getWorldSize()
     const circles: Circle[] = []
     for (let i = 0; i < n; i++) {
-        const x = Math.round(Math.random() * 3800) + 150
-        const y = Math.round(Math.random() * 2000) + 150
         const r = Math.round(Math.random() * 100) + 50
+        const x = Math.round(Math.random() * (worldSize.width - r * 2)) + r
+        const y = Math.round(Math.random() * (worldSize.height - r * 2)) + r
         const color = colors[Math.round(Math.random() * colors.length)]
         const circle = new Circle({ position: { x, y }, radius: r, fill: color })
         circle.forces.push(new Gravity({ x: 0, y: 100 }))
