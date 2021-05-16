b5872b34f213ac3340cdfccc8a3b582d0a02bf8f
Luke Millar <luketmillar@gmail.com>
2021-05-15 21:46:35 -0700

    Drop balls after 2 seconds

--- src/projects/fallingBalls/Controller.ts
+++ src/projects/fallingBalls/Controller.ts
@@ -3,10 +3,9 @@ import { Position } from "../canvasScene/types"
 import FallingBallsWorld from "./World"
 
 export default class FallingBallsController extends Controller<FallingBallsWorld> {
+    private dropped = false
     public onClick = (position: Position) => {
-        this.world.balls.forEach(ball => {
-            ball.gravity = { x: 0, y: 4000 }
-        })
+        this.drop()
     }
 
     public onMouseMove = (position: Position) => {
@@ -15,5 +14,15 @@ export default class FallingBallsController extends Controller<FallingBallsWorld
     public start = () => {
         super.start()
         this.world.start(this.runTime)
+    }
+
+    public drop = () => {
+        if (this.dropped) {
+            return
+        }
+        this.dropped = true
+        this.world.balls.forEach(ball => {
+            ball.gravity = { x: 0, y: 4000 }
+        })
     }
 }
